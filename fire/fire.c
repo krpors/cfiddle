28fcbdf4e1a6bed53e57aac6a6939e98dced9511
Kevin Pors <krpors@gmail.com>
2018-07-28 00:30:14 +0200

    create struct matrix

--- fire/fire.c
+++ fire/fire.c
@@ -1,12 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <inttypes.h>
 #include <time.h>
 
 #include <SDL.h>
 
 inline int color() {
 	return rand() % 255;
+}
+
+/*
+ * A pixel matrix, where each element in the array represents
+ * a uint32_t RGB value.
+ */
+struct matrix {
+	uint32_t* array;
+	int width;
+	int height;
+};
+
+void matrix_init(struct matrix* m, int width, int height) {
+	m->array = malloc(width * height * sizeof(uint32_t));
+}
+
+void matrix_free(struct matrix* m) {
+	free(m->array);
+}
+
+inline uint32_t matrix_get(struct matrix* m, int x, int y) {
+	return m->array[y * m->width + x % m->width];
+}
+
+inline void matrix_set(struct matrix* m, int x, int y, uint32_t val) {
+	m->array[y * m->width + x] = val;
+}
+
+void matrix_update(struct matrix* m) {
+	// initialize the bottom row with cruft.
+	for (int x = 0; x < m->width; x++) {
+		matrix_set(m, x, m->height - 2, color());
+	}
+
+	// calculate the rest
+	for (int y = 0; y < m->height - 2; y++) {
+		for (int x = 0; x < m->width; x++) {
+			short bl = matrix_get(m, x - 1, y + 1);
+			short br = matrix_get(m, x + 1, y + 1);
+			short ib = matrix_get(m, x    , y + 1);
+			short bb = matrix_get(m, x    , y + 2);
+			short newval = (bl + br + ib + bb) / 4.04;
+			matrix_set(m, x, y, newval);
+		}
+	}
 }
 
 const int width = 100;
@@ -76,24 +122,20 @@ void burn(SDL_Surface* target, short* array) {
 	}
 }
 
-int _main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 	(void)(argc);
 	(void)(argv);
 
 	srand(time(NULL));
 
-	short* ss = malloc(width * height * sizeof(short));
-	memset(ss, 0, width * height * sizeof(short));
+	struct matrix m;
+	matrix_init(&m, 20, 20);
+	matrix_free(&m);
 
-	update(ss);
-	update(ss);
-	update(ss);
-
-	free(ss);
 	return 0;
 }
 
-int main(int argc, char* argv[]) {
+int _main(int argc, char* argv[]) {
 	(void)(argc);
 	(void)(argv);
 
@@ -157,4 +199,5 @@ int main(int argc, char* argv[]) {
 	SDL_FreeSurface(surface);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	return 0;
 }
